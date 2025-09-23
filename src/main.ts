e5f8e009c264f6bf34d3f22a81454e5ad5a64ffa
hch-prog <chaniyara.harsh26@gmail.com>
2025-09-24 00:33:18 +0530

    vercel config

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   // Use ConfigService to fetch PORT from .env
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT', 3001);
+  const port = configService.get<number>('PORT') || 3001;
 
   await app.listen(port);
   console.log(`🚀 Application is running on: http://localhost:${port}`);
